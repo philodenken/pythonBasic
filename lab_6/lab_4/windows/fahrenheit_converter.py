7e7243335ad22169bc63af1aab71e520288dc0ce
Jaeyoung Yoon <magenta_jy@naver.com>
2018-01-02 14:58:30 +0900

    add python program for learning how to use console

---
+++ lab_6/lab_4/windows/fahrenheit_converter.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+def input_celsius_value():
+    print("변환하고 싶은 섭씨온도를 입력해 주세요: ")
+    user_input = float(input())
+    return user_input
+
+def convert_celsius_fahrenheit(celsius_value):
+    fahrenheit_value = (((9/5)*celsius_value)+32)
+    return fahrenheit_value
+
+def print_fahrenheit_value(celsius_value, fahrenheit_value):
+    print("섭씨온도 :", str(celsius_value))
+    print("화씨온도 :", str(fahrenheit_value))
+
+def main():
+    print("본 프로그램은 섭씨를 화씨로로 변환해주는 프로그램입니다")
+    print("============================")
+    # ===Modify codes below=================
+    celsius_value = input_celsius_value()
+    fahrenheit_value = convert_celsius_fahrenheit(celsius_value)
+    print_fahrenheit_value(celsius_value, fahrenheit_value)
+    # ======================================
+    print("===========================")
+    print("프로그램이 종료 되었습니다.")
+
+
+if __name__ == '__main__':
+    main()
